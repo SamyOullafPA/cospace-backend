65681d7cc4f4ead8ed2ee501bdbdc304315baa39
Samy Oullaf <samy.oullaf@paconsulting.com>
2026-09-16 16:45:36 +0100

    feat: Created migration SQL scripts

---
+++ migrations/001_init_schema.down.sql
@@ -0,0 +1,5 @@
+DROP TABLE users;
+DROP TABLE teams;
+DROP TABLE rooms;
+DROP TABLE desks;
+DROP TABLE bookings;
---
+++ migrations/001_init_schema.up.sql
@@ -0,0 +1,35 @@
+CREATE TABLE teams(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    department TEXT NOT NULL
+);
+
+CREATE TABLE users(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    team_id INT,
+    FOREIGN KEY (team_id) REFERENCES teams(id)
+);
+
+CREATE TABLE rooms(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    floor int NOT NULL, 
+    capacity int NOT NULL
+);
+
+CREATE TABLE desks(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    floor int NOT NULL
+);
+
+CREATE TABLE bookings(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT,
+    desk_id INT,
+    booking_date date NOT NULL,
+    active boolean NOT NULL
+);
