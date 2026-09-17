feace85ad181ed96631abaf15b1cc49cbc217c8f
Samy Oullaf <samy.oullaf@paconsulting.com>
2026-09-17 11:15:39 +0100

    fix: Supports dropping of tables with foreign key constraints

--- migrations/002_add_indexing.down.sql
+++ migrations/002_add_indexing.down.sql
@@ -1,1 +1,9 @@
+ALTER TABLE bookings
+DROP FOREIGN KEY bookings_ibfk_2;
+
 DROP INDEX DUPLICATE_DESK_INDEX ON bookings;
+
+ALTER TABLE bookings
+ADD CONSTRAINT bookings_ibfk_2
+FOREIGN KEY (desk_id)
+REFERENCES desks(id);
