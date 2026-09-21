7145ab4a6c3f96c1c245eac216ac047dedae9ccd
Samy Oullaf <samy.oullaf@paconsulting.com>
2026-09-21 17:18:23 +0100

    fix: Made setting up database idempotent

--- setup_database.sh
+++ setup_database.sh
@@ -1,4 +1,4 @@
-mysql -u root -p -e  "CREATE DATABASE cospace;"
+mysql -u root -p -e  "CREATE DATABASE IF NOT EXISTS cospace;"
 mysql -u root -p cospace < migrations/001_init_schema.up.sql
 mysql -u root -p cospace < migrations/002_add_indexing.up.sql
 mysql -u root -p cospace < scripts/seed_and_queries.sql
